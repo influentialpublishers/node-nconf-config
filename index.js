c76ceaf8e6645ea2f25fde4a5da9e1e094286720
Asa Ayers <aayers@influential.co>
2017-09-14 14:05:12 -0700

    decaffeinate

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-require('coffee-script/register');
-module.exports  = require( __dirname + '/lib/config');
+const nconf   = require('nconf');
+
+const _loadFileIntoConfig = (config, path, type) => config.file(type, `${path}/${type}.json`);
+
+const _isVpn = function(config) {
+  config = config.get();
+  return config.VPN;
+};
+
+module.exports  = function(config) {
+  let resolved = nconf.argv();
+  resolved = resolved.env('__');
+
+  const base = config.base_path;
+  if (_isVpn(resolved)) {
+    resolved = _loadFileIntoConfig(resolved, base, 'vpn_environment');
+    resolved = _loadFileIntoConfig(resolved, base, 'vpn');
+  }
+
+  resolved = _loadFileIntoConfig(resolved, base, 'environment');
+  resolved = _loadFileIntoConfig(resolved, base, 'defaults');
+
+  return resolved.get();
+};
